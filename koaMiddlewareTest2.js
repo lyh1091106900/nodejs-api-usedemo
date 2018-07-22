797e1187ee00c8ad0f427711f2417e04ac223077
lyh1091106900 <lyh1091106900@gmail.com>
2018-07-23 00:56:49 +0800

    Create koaMiddlewareTest2.js

---
+++ koaMiddlewareTest2.js
@@ -0,0 +1,56 @@
+/*
+输出
+action 001
+action 002
+action 003
+action 004
+action 005
+action 006
+end
+context =  { data: [ 1, 2, 3, 4, 5, 6 ] }
+
+*/
+
+let context = {
+  data: []
+};
+
+async function middleware1(ctx, next) {
+  console.log('action 001');
+  ctx.data.push(1);
+  await next();
+  console.log('action 006');
+  ctx.data.push(6);
+}
+
+async function middleware2(ctx, next) {
+  console.log('action 002');
+  ctx.data.push(2);
+  await next();
+  console.log('action 005');
+  ctx.data.push(5);
+}
+
+async function middleware3(ctx, next) {
+  console.log('action 003');
+  ctx.data.push(3);
+  await next();
+  console.log('action 004');
+  ctx.data.push(4);
+}
+
+Promise.resolve(middleware1(context, async() => {
+  return Promise.resolve(middleware2(context, async() => {
+    return Promise.resolve(middleware3(context, async() => {
+      return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("step1")
+        }, 2000)
+    });
+    }));
+  }));
+}))
+  .then(() => {
+    console.log('end');
+    console.log('context = ', context);
+  });
